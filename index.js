13be3bf2eeeddc4365a6d1433d247f828f5af359
Antti Alestalo <alestalo.antti@gmail.com>
2021-05-16 21:29:47 +0300

    uibuild

--- index.js
+++ index.js
@@ -15,36 +15,17 @@ morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-
 app.get('/info', (req, res) => {
-    res.send(`<div>
-                <div>Phonebook has info for ${persons.length} people</div>
-                <br/>
-                <div>${new Date()}</div>
-              </div>`)
+    Person.find({})
+        .then(persons => {
+            res.send(`<div>
+                        <div>Phonebook has info for ${persons.length} people</div>
+                        <br/>
+                        <div>${new Date()}</div>
+                    </div>`)
+        })
+        .catch(err => next(error))
+
 })
 
 app.get('/api/persons', (req, res) => {
@@ -52,53 +33,76 @@ app.get('/api/persons', (req, res) => {
         .then(persons => {
             res.json(persons)
         })
-        .catch(err => {
-            console.log(err.message)
-            res.status(404).end()
-        })
+        .catch(err => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
         .then(person => {
-            res.json(person)
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+
         })
-        .catch(err => {
-            console.log(err.message)
-            res.status(404).end()
-        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if (!body.name || !body.number) {
         return res.status(400).json({
             error: 'name or number is missing'
         })
-    } else if (persons.some(p => p.name === body.name)) {
+    } /* else if (persons.some(p => p.name === body.name)) {
         return res.status(400).json({
             error: 'name must be unique'
         })
-    }
+    } */
 
-    const person =  new Person({
+    const person = new Person({
         name: body.name,
         number: body.number,
     })
 
-    person.save().then(response => {
-        console.log(`added ${response.name} number ${response.number} to phonebook`)
-        res.json(response)
-    })
+    person.save()
+        .then(response => {
+            console.log(`added ${response.name} number ${response.number} to phonebook`)
+            res.json(response)
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number is missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => {
+            res.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 
@@ -106,7 +110,21 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
